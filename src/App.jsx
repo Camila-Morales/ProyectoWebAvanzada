6d37d6030dbb131b77c83fdcfbfce7da3040aab3
Antoni30 <artoapanta3@espe.edu.ec>
2023-12-15 12:01:32 -0500

    AddTest

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import TestimonioImgP from "./components/moleculas/TestimonioImgP"
+import TestimonioImgP from "./components/moleculas/DescriptionTestimonio"
+import Avatr from '/Avatar.jpg'
 
 function App() {
   let parrafo="Quiero expresar mi gratitud hacia GYM RAT por haber transformado mi vida de una manera que nunca imaginé. Antes de unirme, me encontraba en una rutina sedentaria y me sentía sin energía. Sin embargo, desde el momento en que ingresé a este gimnasio, todo cambió";
-  let imgaDir= "https://sm.ign.com/t/ign_es/screenshot/default/avatarjpg-b21b3a_125372_6bdh.1280.jpg";
   let Alt="Imagen Avatar";
   let name= "Camila";
   let plan= "Mamadisima"
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <h1>SOY APP PRINCIPAL</h1>
-      <TestimonioImgP texto={parrafo}  dirImg={imgaDir} altImg={Alt} avatarName= {name} plan={plan}/>
+      <TestimonioImgP texto={parrafo}  dirImg={Avatr} altImg={Alt} avatarName= {name} plan={plan}/>
     </>
   )
 }
---
+++ src/components/organismos/Testimonio.jsx
@@ -0,0 +1,7 @@
+function Testimonio() {
+    return (<>
+    <h1>Tesmonio</h1>
+    </>  );
+}
+
+export default Testimonio;
