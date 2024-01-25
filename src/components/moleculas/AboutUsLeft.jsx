4b4e65f9f0f54dace5155407f663121bdae6e41b
Camila-Morales <scmorales1@espe.edu.ec>
2024-01-25 08:26:26 -0500

    AboutUs

--- src/components/moleculas/AboutUsLeft.jsx
+++ src/components/moleculas/AboutUsLeft.jsx
@@ -4,14 +4,16 @@ import Imagenizq from '../../../public/imgizq.png';
 
 function AboutUsLeft() {
   return (
-    <div className="flex justify-center items-center space-x-8 relative">
-      <div className="w-1/4">
-        <Image src={Imagender} alt="img derecha" />
+    <div className="flex flex-col items-center sm:flex-row sm:justify-center space-y-4 sm:space-x-8 relative">
+
+      <div className="w-full sm:w-1/2 md:w-1/4">
+        <Image src={Imagender} alt="img derecha" className="w-full h-auto sm:max-w-xs" />
       </div>
 
-      <div className="w-1/4">
-        <Image src={Imagenizq} alt="img izquierda" />
+      <div className="w-full sm:w-1/2 md:w-1/4">
+        <Image src={Imagenizq} alt="img izquierda" className="w-full h-auto sm:max-w-xs" />
       </div>
+
     </div>
   );
 }
--- src/components/moleculas/AboutUsRight.jsx
+++ src/components/moleculas/AboutUsRight.jsx
@@ -3,18 +3,18 @@ import Paragraph from "../atomos/Paragraph";
 
 function AboutUsRight() {
   return (
-    <>
-      <div className="text-4xl font-bold text-center mb-4">
+    <div className="text-center mx-4 md:mx-8 lg:mx-16 xl:mx-20 2xl:mx-32">
+      <div className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4">
         <AboutUsH1 />
       </div>
 
       <div className="mb-4">
         <Paragraph
-          styles={"text-xl italic text-justify"}
-          texto="En GYM R.A.T, nos enorgullece ser más que un gimnasio; somos una comunidad comprometida con la excelencia en el fitness y el bienestar. Nuestras instalaciones de última generación ofrecen un entorno propicio para el crecimiento personal, respaldado por entrenadores altamente calificados y programas personalizados que se adaptan a tus metas individuales. Valoramos la camaradería y la motivación, creando un ambiente donde los miembros se inspiran mutuamente a alcanzar nuevos niveles de salud y rendimiento."
+          styles="text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl italic text-justify"
+          texto="En GYM R.A.T, nos enorgullece ser más que un gimnasio; somos una comunidad comprometida con la excelencia en el fitness y el bienestar. Nuestras instalaciones de última generación ofrecen un entorno propicio para el crecimiento personal, respaldado por entrenadores altamente calificados y programas personalizados que se adaptan a tus metas individuales."
         />
       </div>
-    </>
+    </div>
   );
 }
 
