cd985f096f8beee2b5c165055e9fcdd8ee7097a4
Antoni30 <artoapanta3@espe.edu.ec>
2024-02-02 23:20:41 -0500

    auth user

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppI from "./Pages/AppPage.jsx";
 import Login from "./Pages/LoginPage.jsx";
 import Profile from "./Pages/ProfilePage.jsx";
 import ProtectedRout from "./ProtectedRout.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 function App() {
   return (
     <>
-          <AuthProvider>
-              <BrowserRouter>
-                  <Routes>
-                    <Route path="/" element={<AppI/>}/>
-                    <Route path="/login" element={<Login/>}/>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppI />} />
+            <Route path="/login" element={<Login />} />
 
-                    <Route element={<ProtectedRout/>}>
-                      <Route path="/profile" element={<Profile/>}/>
-                    </Route>
-                  </Routes>
-              </BrowserRouter>
-          </AuthProvider>
+            <Route element={<ProtectedRout />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- Frontend/src/components/moleculas/Login.jsx
+++ Frontend/src/components/moleculas/Login.jsx
@@ -1,34 +1,30 @@
 import { useForm } from "react-hook-form";
-import { useAuth } from "../../context/AuthContext";
+import useAuth from "../../hooks/useAuth";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 function Login({ registerF }) {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const { signup, isAuth,errors:AuthError} = useAuth();
+  const { signin, isAuthenticated, error } = useAuth();
   const navigate = useNavigate();
   const handleActivate = (e) => {
     e.preventDefault();
     registerF();
   };
   const onSubmit = handleSubmit((value) => {
-    signup(value);
+    signin(value);
   });
   useEffect(() => {
-    if (isAuth) navigate("/profile");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuth]);
+    if (isAuthenticated) navigate("/profile");
+  }, [isAuthenticated, navigate]);
   return (
     <div className=" relative w-[28rem] h-[30rem] text-white bg-red-700 flex flex-col  gap-5 justify-center items-center ">
       {
-        AuthError.map((err,i)=>(
-          <div key={i} className="absolute top-0 text-center bg-red-500 p-1 w-full text-white">
-            {err}
-          </div>
-        ))
+        error.map((err,index)=>(<div className="absolute top-0 bg-blue-800 w-full flex justify-center items-center h-8" key={index}>{err}</div>))
       }
       <h1 className="absolute top-9 text-center">
         <strong className="text-xl">Iniciar Seción</strong>
@@ -43,11 +39,11 @@ function Login({ registerF }) {
             id="userName"
             {...register("userName", { required: true })}
           />
-          {
-            errors.userName &&(<p className="absolute top-[10rem] text-blue-950">
+          {errors.userName && (
+            <p className="absolute top-[10rem] text-blue-950">
               UserName is Required
-            </p>)
-          }
+            </p>
+          )}
         </div>
         <div className="flex flex-col gap-3">
           <label htmlFor="passw">Contraseña</label>
@@ -57,11 +53,11 @@ function Login({ registerF }) {
             id="passw"
             {...register("password", { required: true })}
           />
-          {
-            errors.password &&(<p className="absolute top-[17rem] text-blue-950">
+          {errors.password && (
+            <p className="absolute top-[17rem] text-blue-950">
               Password is Required
-            </p>)
-          }
+            </p>
+          )}
         </div>
         <button
           className="bg-red-900 h-12 rounded-3xl hover:bg-red-950"
